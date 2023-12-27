334eea5b1c5bc1b63042da43401c8aaa9aedc873
Vadim Voitsehovskiy <vadim.rolex.2005@gmail.com>
2023-12-27 16:21:18 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace lab_6._2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(70, 63);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(246, 34);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(404, 24);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(733, 670);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(70, 578);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 34);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox2.Location = new System.Drawing.Point(188, 578);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 34);
+            this.textBox2.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(67, 615);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 16);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "ширина";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(185, 615);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 16);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "висота";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(70, 647);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(218, 47);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Задати розмір";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1169, 720);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab_6._2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedIndex == 0)
+            {
+                pictureBox1.Image = Image.FromFile("C:\\Documents\\4 курс\\1 семестр\\Інструментальні засоби візуального програмування\\lab_6.2\\img1.jpg");
+                pictureBox1.SizeMode = PictureBoxSizeMode.Zoom; // змінюємо режим зображення
+                pictureBox1.Size = new Size(150, 100); // зміна розмірів pictureBox
+            }
+            else if (comboBox1.SelectedIndex == 1)
+            {
+                pictureBox1.Image = Image.FromFile("C:\\Documents\\4 курс\\1 семестр\\Інструментальні засоби візуального програмування\\lab_6.2\\img1.jpg");
+                pictureBox1.SizeMode = PictureBoxSizeMode.Zoom; // змінюємо режим зображення
+                pictureBox1.Size = new Size(300, 200); // зміна розмірів pictureBox
+            }
+            else if (comboBox1.SelectedIndex == 2)
+            {
+                pictureBox1.Image = Image.FromFile("C:\\Documents\\4 курс\\1 семестр\\Інструментальні засоби візуального програмування\\lab_6.2\\img1.jpg");
+                pictureBox1.SizeMode = PictureBoxSizeMode.Zoom; // змінюємо режим зображення
+                pictureBox1.Size = new Size(600, 400); // зміна розмірів pictureBox
+            }
+            else
+            {
+                // якщо інший елемент вибраний, очистити PictureBox
+                pictureBox1.Image = null;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox1.Items.Add("Маленьке");
+            comboBox1.Items.Add("Середнє");
+            comboBox1.Items.Add("Велике");
+            comboBox1.SelectedIndex = 0;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int width, height; // оголошення змінних для збереження ширини і висоти
+
+            if (int.TryParse(textBox1.Text, out width) && int.TryParse(textBox2.Text, out height)) // перетворення введених значень ширини та висоти в цілі числа
+            {
+                Image img = Image.FromFile("C:\\Documents\\4 курс\\1 семестр\\Інструментальні засоби візуального програмування\\lab_6.2\\img1.jpg"); // завантаження фото з файлу
+
+                pictureBox1.Size = new Size(width, height); // встановлення розмірів pictureBox1
+
+                pictureBox1.SizeMode = PictureBoxSizeMode.Zoom; // встановлення режиму масштабування зображення
+
+                double aspectRatio = (double)img.Width / img.Height; // визначення співвідношення сторін зображення
+
+                if (width > height) // перевірка, яка зі сторін більша, і зміна розмірів pictureBox1 відповідно
+                {
+                    pictureBox1.Size = new Size(width, (int)(width / aspectRatio));
+                } 
+                else if (height > width)
+                {
+                    pictureBox1.Size = new Size((int)(height * aspectRatio), height);
+                }
+                pictureBox1.Image = img; // встановлення зображення для pictureBox1
+            }
+            else
+            {
+                MessageBox.Show("Будь ласка, введіть коректні значення для ширини і висоти."); 
+                // виведення повідомлення про помилку, якщо введені значення некоректні або взагалі ми їх не ввели
+            }
+        }
+    }
+}
